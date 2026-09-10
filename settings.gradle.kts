647f08c812d2cded679caf49fe1eb1c879b83bed
AhmadKharfan <plamoplamo12@gmail.com>
2026-09-10 04:53:00 +0300

    chore(build): add settings and root build script

---
+++ settings.gradle.kts
@@ -0,0 +1,16 @@
+rootProject.name = "wird"
+
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}

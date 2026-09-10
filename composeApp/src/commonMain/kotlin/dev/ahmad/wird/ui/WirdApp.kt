25df49d07e55645a081b1fde10ca1db4f5cbbbc4
AhmadKharfan <plamoplamo12@gmail.com>
2026-09-10 04:56:35 +0300

    feat(ui): add shared app root composable

---
+++ composeApp/src/commonMain/kotlin/dev/ahmad/wird/ui/WirdApp.kt
@@ -0,0 +1,12 @@
+package dev.ahmad.wird.ui
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+
+@Composable
+fun WirdApp() {
+    MaterialTheme {
+        Surface {}
+    }
+}
